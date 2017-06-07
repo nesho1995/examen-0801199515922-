e9a6e72f670d7597c33acf2e943def1b20790d0c
Unknown <nhernandezosorto@gmail.com>
2017-06-06 19:44:20 -0600

    suma y muestra de datos

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,28 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/0801199515922', function(req,res,next){
+  var examen = [
+                {"nombre":"Nestor Josue Hernandez Osorto"},
+                {"cuenta":0801199515922},
+                  {"correo":"nhernandezosorto@gmail.com"},
+              ];
+  res.json(examen);
+});
+router.get('/suma', function(req,res,next){
+  res.render('suma',{"txtNum1":"","txtNum2":"","sum":""});
+});
+
+
+var suma= [];
+router.post('/suma',function(req,res,next){
+  console.log(req.body);
+      var nume1=req.body.txtNum1;
+      var nume2=req.body.txtNum2;
+      suma.push(parseInt(nume1)+parseInt(nume2))
+      var resultado=suma;
+      var sumar={"sum":resultado}
+      res.render('suma',sumar);
+});
 
 module.exports = router;
